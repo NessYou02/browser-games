5a68cdb1852b1288019eff98aa69ad18ca05a753
Youness <younes.benchikh.02@gmail.com>
2025-01-03 14:20:28 +0100

    modified vue config file

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+    //publicPath: process.env.NODE_ENV === 'production' ? '/browser-games/' : '/'
+    publicPath: './',
+    outputDir:'dist',
+    assetsDir:'static'
 })
